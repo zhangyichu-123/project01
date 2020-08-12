e18da71a34f6b1f471d24f090bf1dafe58db8d07
zhangyichu-123 <69244539+zhangyichu-123@users.noreply.github.com>
2020-08-12 14:46:36 +0800

    Add files via upload
    init

---
+++ src/com/taobao/test01.java
@@ -0,0 +1,12 @@
+package com.taobao;
+
+public class test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("无敌是多么寂寞");
+		
+		System.out.println("222222");
+	}
+
+}
---
+++ src/com/taobao/tset02.java
@@ -0,0 +1,14 @@
+package com.taobao;
+
+public class tset02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("1111111111");
+		
+		System.out.println("2346411");
+	}
+
+}
